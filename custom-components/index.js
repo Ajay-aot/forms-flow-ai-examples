21974e61778be7ca87fce1053ccec2eea0b6000c
Ajay krishna <ajay.k@aot-technologies.com>
2023-03-06 15:57:37 +0530

    video embedding

--- custom-components/index.js
+++ custom-components/index.js
@@ -1,9 +1,11 @@
 import fileUpload from "./custom-file-upload/fileUpload";
 import Maps from './google-map-component/GMapservice';
+import VideoEmbed from "./VideoEmbedding/videoEmbed";
 
 const components = {
   fileUpload: fileUpload,
   Maps: Maps,
+  videoEmbed : VideoEmbed
 };
 
 export default components;
